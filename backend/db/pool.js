6b0484ac5946d49b7c5e3f556330c4f938ade5d4
Pritam <kumarpreetjangir@gmail.com>
2026-02-08 14:34:13 +0530

    environment bug fix

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,15 +1,12 @@
 const {Pool} = require('pg');
 require("dotenv").config();
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   });
+  
   
 
 module.exports = pool;
